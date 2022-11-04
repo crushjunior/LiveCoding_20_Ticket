e67e5e1c43d833d299e9dd5a64f63d064cb3ab2f
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-11-04 15:32:05 +0300

    Дан массив строк, необходимо удалить из него все дубли, оставшиеся строки объединить в одну в порядке следования в массиве.

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -1,7 +1,18 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        String[] str = {"One", "Two", "Three", "Four", "One", "Five", "Two"};
 
+        Set<String> set = new LinkedHashSet<>(Arrays.asList(str));
+        StringBuilder result = new StringBuilder();
+        for (String string : set) {
+            result.append(string);
+        }
+        System.out.println(result);
     }
 }
