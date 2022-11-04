e63ddc01395bdf5f5a00d7a3bcd127ba7f6f0064
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-11-04 18:56:38 +0300

    Дан List <String> names. Удалите первую букву из каждого имени и верните отсортированный список. ver 2.0

--- src/List/Main.java
+++ src/List/Main.java
@@ -5,17 +5,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
-        List<String> names2 = new ArrayList<>();
         names.add("Петя");
         names.add("Вова");
         names.add("Маша");
 
         System.out.println(names);
-        System.out.println(sortList(names, names2));
+        System.out.println(sortList(names));
 
     }
 
-public static List<String> sortList(List<String> list1, List<String> list2) {
+public static List<String> sortList(List<String> list1) {
+    List<String> list2 = new ArrayList<>();
         for (String string : list1) {
         list2.add(string.substring(1));
         }
